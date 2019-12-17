fb94cc7c249331d68a239a437378fe4c4944392e
Arjunan Ravi <arjunanatwork@gmail.com>
2019-12-17 21:54:09 +0400

    Adding Other Action Icons ( Move & Schedule ), Incrementing Font Awesome Version

--- src/components/edit-task/edit-task.component.jsx
+++ src/components/edit-task/edit-task.component.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import {useDispatch} from "react-redux";
 import {toggleEditTask, updateTaskStartAsync} from "../../redux/task/task.action";
 
+import './edit-task.component.scss';
+
 const EditTask = ({task}) => {
 
     const dispatch = useDispatch();
@@ -21,10 +23,18 @@ const EditTask = ({task}) => {
     return (
         <div className="edit-task-detail has-margin-bottom-20">
             <input className="input" type="text" placeholder="Task Detail" value={taskDetail} onChange={(e) => setTaskDetail(e.target.value)}/>
-            <div className="edit-task-action">
-                <div className="buttons">
+            <div className="edit-task-action-container">
+                <div className="buttons is-marginless">
                     <button className="button is-primary" onClick={updateTask}>Update Task</button>
                     <button className="button" onClick={cancelUpdate}>Cancel</button>
+                </div>
+                <div className="other-actions">
+                    <span className="icon">
+                      <i className="fas fa-list fa-lg"></i>
+                    </span>
+                    <span className="icon">
+                      <i className="fas fa-calendar-day fa-lg"></i>
+                    </span>
                 </div>
             </div>
         </div>
