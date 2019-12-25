ef0c3ee70189de9633668557eb87abf0cef4d81b
Arjunan Ravi <arjunanatwork@gmail.com>
2019-12-25 23:21:08 +0400

    Fixing Warning

--- src/components/switch-project-dropdown-items/switch-project-dropdown-items.component.jsx
+++ src/components/switch-project-dropdown-items/switch-project-dropdown-items.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SwitchProjectDropdownItem = ({ project, changeProject}) => {
     return (
-        <a href="javascript:void(0);" className="dropdown-item" onClick={() => changeProject(project.id)}>{project.name}</a>
+        <a href="#" className="dropdown-item" onClick={() => changeProject(project.id)}>{project.name}</a>
     )
 };
 
